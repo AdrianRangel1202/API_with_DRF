dacde3c2f1818b1bf418063161eaf237034c3859
Adrian <adrianrangel1202@gmail.com>
2024-03-31 00:16:14 -0400

    Creacion de los Metodos PUT y DELETE

--- ecomerce/apps/users/api/views.py
+++ ecomerce/apps/users/api/views.py
@@ -13,11 +13,14 @@ class UserAPIView(APIView):
 
             queryset = User.objects.all()
             filtered_queryset = self.filter_queryset(queryset)
+
             if filtered_queryset:
                 serializer = UserSerializer(filtered_queryset, many=True)
                 return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+            
             elif not filtered_queryset :
                 return Response({'Error':'User Not Found'}, status=status.HTTP_404_NOT_FOUND)
+            
             else:
                 users = User.objects.all()
                 user_serializer = UserSerializer(users, many = True)
@@ -39,3 +42,18 @@ class UserAPIView(APIView):
             return queryset.filter(username__icontains=search_query)
         return queryset
 
+    def put(self, request):
+        if request.method == "PUT":
+            user = User.objects.filter(username= request.data["username"]).first()
+            user_serializer =  UserSerializer(user, data = request.data)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return Response(user_serializer.data, status=status.HTTP_202_ACCEPTED)
+            return Response(user_serializer.errors)
+        
+    def delete(self,request):
+
+        if request.method == "DELETE":      
+            user = User.objects.filter(username = self.request.GET.get("username")).first()
+            user.delete()
+            return Response({'menssage':'user successfully deleted'}, status=status.HTTP_200_OK)
