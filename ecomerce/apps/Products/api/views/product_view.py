eab1544ef95680c378b81dfa35e272c054ac5dc2
Adrian <adrianrangel1202@gmail.com>
2024-05-07 10:32:40 -0400

    autenticacion con simplejwt en global

--- ecomerce/apps/Products/api/views/product_view.py
+++ ecomerce/apps/Products/api/views/product_view.py
@@ -1,14 +1,12 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 from apps.Products.api.serializers.product_serializer import ProductSerializer
 
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
-    permission_classes = (IsAuthenticated,)
 
 
     def get_queryset(self, pk = None):
